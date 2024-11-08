827b99f198fdf82a112337a382d6183e6f09c5b2
SunnyKumar-code <sk202542@gmail.com>
2024-11-08 13:45:34 +0530

    edit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+
     let url = 'https://api.unsplash.com/search/photos/?client_id=Bn0EgsVvcO3KK0Mh5uX_1tzHgMXAaAkDVJJaUAJLHQk&query=';
     let searchText = document.getElementById('searchInput');
     let searchbtn = document.getElementById('submit');
@@ -49,4 +49,4 @@ document.addEventListener('DOMContentLoaded', () => {
             imageList.appendChild(li);
         });
     }
-});
+
